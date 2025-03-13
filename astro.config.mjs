174063072d04e21acfbad2110bd3b6d908badcf1
Vincent Cayadi <cayadivincent@gmail.com>
2025-03-13 13:48:06 +0800

    typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   markdown: {
     shikiConfig: {
-      themes: { light: "min-light", dark: "	kanagawa-dragon" },
+      themes: { light: "min-light", dark: "kanagawa-dragon" },
     },
   },
   site: "https://vincentcayadi.github.io/",
