a52c64a05745e8c67e861522d113d9c64b1cbc10
Vincent Cayadi <cayadivincent@gmail.com>
2025-03-13 10:58:06 +0800

    shiki theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,11 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      theme: "kanagawa-dragon",
+    },
+  },
   site: "https://vincentcayadi.github.io/",
   integrations: [mdx(), sitemap(), tailwind()],
 });
